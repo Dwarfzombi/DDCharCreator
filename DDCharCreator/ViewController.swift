adfb9106662425fbbaea5270f29c3bed347a60a5
Hussam Azab <227491@s020rmc148-08.local>
2018-04-13 13:39:06 -0500

    Initial Commit

---
+++ DDCharCreator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DDCharCreator
+//
+//  Created by Azab, Hussam M on 4/13/18.
+//  Copyright © 2018 Azab, Hussam M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
